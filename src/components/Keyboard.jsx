00cc2cabc7dadc305d16ad554823a1f5b8282db4
Ryne <ryne@itsrynenotryan.com>
2022-05-16 13:27:54 -0700

    chore(Keyboard): Update Mac keyboard layout, tweak styles, add react icons

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,94 +1,261 @@
+import {
+  BsCommand,
+  BsOption,
+  BsShiftFill,
+  BsCapslockFill,
+  BsFillBackspaceFill,
+} from 'react-icons/bs';
+import { ImCtrl } from 'react-icons/im';
+import {
+  MdKeyboardTab,
+  MdOutlineKeyboardReturn,
+  MdPowerSettingsNew,
+} from 'react-icons/md';
+
 const Keyboard = () => {
   return (
-    <div class="keyboard layout_mac">
-      <button class="key col-span-2">~</button>
-      <button class="key col-span-2">1</button>
-      <button class="key col-span-2">2</button>
-      <button class="key col-span-2">3</button>
-      <button class="key col-span-2">4</button>
-      <button class="key col-span-2">5</button>
-      <button class="key col-span-2">6</button>
-      <button class="key col-span-2">7</button>
-      <button class="key col-span-2">8</button>
-      <button class="key col-span-2">9</button>
-      <button class="key col-span-2">0</button>
-      <button class="key col-span-2">-</button>
-      <button class="key col-span-2">+</button>
-      <button class="key col-span-4" title="delete">
+    <div className="keyboard layout_mac">
+      <button className="key col-span-3" title="Esc">
+        esc
+      </button>
+      <button className="key col-span-2" title="F1">
+        F1
+      </button>
+      <button className="key col-span-2" title="F2">
+        F2
+      </button>
+      <button className="key col-span-2" title="F3">
+        F3
+      </button>
+      <button className="key col-span-2" title="F4">
+        F4
+      </button>
+      <button className="key col-span-2" title="F5">
+        F5
+      </button>
+      <button className="key col-span-2" title="F6">
+        F6
+      </button>
+      <button className="key col-span-2" title="F7">
+        F7
+      </button>
+      <button className="key col-span-2" title="F8">
+        F8
+      </button>
+      <button className="key col-span-2" title="F9">
+        F9
+      </button>
+      <button className="key col-span-2" title="F10">
+        F10
+      </button>
+      <button className="key col-span-2" title="F11">
+        F11
+      </button>
+      <button className="key col-span-2" title="F12">
+        F12
+      </button>
+      <button className="key col-span-3" title="Power button">
+        <MdPowerSettingsNew className="text-lg" />
+      </button>
+      <button className="key col-span-2" title="tilde">
+        ~
+      </button>
+      <button className="key col-span-2" title="1">
+        1
+      </button>
+      <button className="key col-span-2" title="2">
+        2
+      </button>
+      <button className="key col-span-2" title="3">
+        3
+      </button>
+      <button className="key col-span-2" title="4">
+        4
+      </button>
+      <button className="key col-span-2" title="5">
+        5
+      </button>
+      <button className="key col-span-2" title="6">
+        6
+      </button>
+      <button className="key col-span-2" title="7">
+        7
+      </button>
+      <button className="key col-span-2" title="8">
+        8
+      </button>
+      <button className="key col-span-2" title="9">
+        9
+      </button>
+      <button className="key col-span-2" title="0">
+        0
+      </button>
+      <button className="key col-span-2" title="-">
+        -
+      </button>
+      <button className="key col-span-2" title="+">
+        +
+      </button>
+      <button className="key col-span-4 pt-1" title="delete">
+        <BsFillBackspaceFill />
         delete
       </button>
-      <button class="key col-span-3">tab</button>
-      <button class="key col-span-2">Q</button>
-      <button class="key col-span-2">w</button>
-      <button class="key col-span-2">E</button>
-      <button class="key col-span-2">R</button>
-      <button class="key col-span-2">T</button>
-      <button class="key col-span-2">Y</button>
-      <button class="key col-span-2">U</button>
-      <button class="key col-span-2">I</button>
-      <button class="key col-span-2">O</button>
-      <button class="key col-span-2">P</button>
-      <button class="key col-span-2">[</button>
-      <button class="key col-span-2">]</button>
-      <button class="key col-span-3">\</button>
-      <button class="modifier col-span-4" title="caps lock">
+      <button className="key col-span-3 pt-1" title="tab">
+        <MdKeyboardTab />
+        tab
+      </button>
+      <button className="key col-span-2" title="q">
+        Q
+      </button>
+      <button className="key col-span-2" title="w">
+        W
+      </button>
+      <button className="key col-span-2" title="e">
+        E
+      </button>
+      <button className="key col-span-2" title="r">
+        R
+      </button>
+      <button className="key col-span-2" title="t">
+        T
+      </button>
+      <button className="key col-span-2" title="y">
+        Y
+      </button>
+      <button className="key col-span-2" title="u">
+        U
+      </button>
+      <button className="key col-span-2" title="i">
+        I
+      </button>
+      <button className="key col-span-2" title="o">
+        O
+      </button>
+      <button className="key col-span-2" title="p">
+        P
+      </button>
+      <button className="key col-span-2" title="left bracket">
+        [
+      </button>
+      <button className="key col-span-2" title="right bracket">
+        ]
+      </button>
+      <button className="key col-span-3" title="backslash">
+        \
+      </button>
+      <button className="modifier col-span-4 pt-1" title="caps lock">
+        <BsCapslockFill />
         caps lock
       </button>
-      <button class="key col-span-2">A</button>
-      <button class="key col-span-2">S</button>
-      <button class="key col-span-2">D</button>
-      <button class="key col-span-2">F</button>
-      <button class="key col-span-2">G</button>
-      <button class="key col-span-2">H</button>
-      <button class="key col-span-2">J</button>
-      <button class="key col-span-2">K</button>
-      <button class="key col-span-2">L</button>
-      <button class="key col-span-2">;</button>
-      <button class="key col-span-2">'</button>
-      <button class="modifier col-span-4">return</button>
-      <button class="modifier col-span-5">shift</button>
-      <button class="key col-span-2">Z</button>
-      <button class="key col-span-2">X</button>
-      <button class="key col-span-2">C</button>
-      <button class="key col-span-2">V</button>
-      <button class="key col-span-2">B</button>
-      <button class="key col-span-2">N</button>
-      <button class="key col-span-2">M</button>
-      <button class="key col-span-2">,</button>
-      <button class="key col-span-2">.</button>
-      <button class="key col-span-2">/</button>
-      <button class="modifier col-span-5" title="shift">
+      <button className="key col-span-2" title="a">
+        A
+      </button>
+      <button className="key col-span-2" title="s">
+        S
+      </button>
+      <button className="key col-span-2" title="d">
+        D
+      </button>
+      <button className="key col-span-2" title="f">
+        F
+      </button>
+      <button className="key col-span-2" title="g">
+        G
+      </button>
+      <button className="key col-span-2" title="h">
+        H
+      </button>
+      <button className="key col-span-2" title="j">
+        J
+      </button>
+      <button className="key col-span-2" title="k">
+        K
+      </button>
+      <button className="key col-span-2" title="l">
+        L
+      </button>
+      <button className="key col-span-2" title="semicolon">
+        ;
+      </button>
+      <button className="key col-span-2" title="">
+        '
+      </button>
+      <button className="modifier col-span-4 pt-1" title="return">
+        <MdOutlineKeyboardReturn />
+        return
+      </button>
+      <button className="modifier col-span-5 pt-1" title="shift">
+        <BsShiftFill />
         shift
       </button>
-      <button class="modifier col-span-2" title="fn">
+      <button className="key col-span-2" title="z">
+        Z
+      </button>
+      <button className="key col-span-2" title="x">
+        X
+      </button>
+      <button className="key col-span-2" title="c">
+        C
+      </button>
+      <button className="key col-span-2" title="v">
+        V
+      </button>
+      <button className="key col-span-2" title="b">
+        B
+      </button>
+      <button className="key col-span-2" title="n">
+        N
+      </button>
+      <button className="key col-span-2" title="m">
+        M
+      </button>
+      <button className="key col-span-2" title="comma">
+        ,
+      </button>
+      <button className="key col-span-2" title="period">
+        .
+      </button>
+      <button className="key col-span-2" title="slash">
+        /
+      </button>
+      <button className="modifier col-span-5 pt-1" title="shift">
+        <BsShiftFill />
+        shift
+      </button>
+      <button className="modifier col-span-2" title="fn">
         fn
       </button>
-      <button class="modifier col-span-2" title="control">
+      <button className="modifier col-span-2 pt-1" title="control">
+        <ImCtrl />
         control
       </button>
-      <button class="modifier col-span-2" title="option">
+      <button className="modifier col-span-2 pt-1" title="option">
+        <BsOption />
         option
       </button>
-      <button class="modifier col-span-3" title="command">
+      <button className="modifier col-span-3 pt-1" title="command">
+        <BsCommand />
         command
       </button>
-      <button class="key col-span-12" title="space bar"></button>
-      <button class="modifier col-span-3" title="command">
+      <button className="key col-span-12" title="space bar"></button>
+      <button className="modifier col-span-3 pt-1" title="command">
+        <BsCommand />
         command
       </button>
-      <button class="modifier col-span-2" title="option">
+      <button className="modifier col-span-2 pt-1" title="option">
+        <BsOption />
         option
       </button>
-      <button class="key col-span-1" title="left arrow">
+      <button className="key col-span-1" title="left arrow">
         &larr;
       </button>
-      <button class="key col-span-1" title="down arrow">
+      <button className="key col-span-1" title="down arrow">
         &darr;
       </button>
-      <button class="key col-span-1" title="up arrow">
+      <button className="key col-span-1" title="up arrow">
         &uarr;
       </button>
-      <button class="key col-span-1" title="right arrow">
+      <button className="key col-span-1" title="right arrow">
         &rarr;
       </button>
     </div>
