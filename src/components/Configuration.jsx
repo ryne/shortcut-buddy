257be9bd7681587e191b355c83ca63a2aaa836cb
Ryne <ryne@itsrynenotryan.com>
2022-05-17 20:40:13 -0700

    chore(Configuration): Handle keyboard layout state, add style logic

--- src/components/Configuration.jsx
+++ src/components/Configuration.jsx
@@ -1,14 +1,28 @@
 import { BsWindows, BsApple } from 'react-icons/bs';
-const Configuration = () => {
+const Configuration = ({ keyboardLayout, setOS }) => {
   return (
     <div
       id="Configuration"
       className="flex justify-start items-center text-slate-800"
     >
-      <button className="toggle_os_mac flex flex-row items-center bg-slate-300 dark:bg-slate-700 dark:text-slate-100 hover:dark:text-slate-100 border-1 py-2 px-4 mx-0.5 rounded-t-lg">
+      <button
+        onClick={() => setOS('mac')}
+        className={`toggle_os_mac flex flex-row items-center py-2 px-4 mx-0.5 rounded-t-lg bg-slate-300 dark:bg-slate-700 dark:text-slate-100 ${
+          keyboardLayout !== 'mac'
+            ? 'bg-slate-200 dark:bg-slate-800 text-slate-400 dark:text-slate-500 hover:text-slate-500 hover:dark:text-slate-400'
+            : ''
+        }`}
+      >
         <BsApple className="mr-2" /> macOS
       </button>
-      <button className="toggle_os_win flex flex-row items-center bg-slate-200 dark:bg-slate-800 text-slate-400 dark:text-slate-500 hover:dark:text-slate-400 border-1 py-2 px-4 mx-0.5 rounded-t-lg">
+      <button
+        onClick={() => setOS('win')}
+        className={`toggle_os_win flex flex-row items-center py-2 px-4 mx-0.5 rounded-t-lg bg-slate-300 dark:bg-slate-700 dark:text-slate-100 ${
+          keyboardLayout !== 'win'
+            ? 'bg-slate-200 dark:bg-slate-800 text-slate-400 dark:text-slate-500 hover:text-slate-500 hover:dark:text-slate-400'
+            : ''
+        }`}
+      >
         <BsWindows className="mr-2" />
         Windows
       </button>
