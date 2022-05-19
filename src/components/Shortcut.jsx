9a68273cdb6cdbab0a63537f0385a550dec000c8
Ryne <ryne@itsrynenotryan.com>
2022-05-18 18:46:50 -0700

    style(Shortcut): Add light theme

--- src/components/Shortcut.jsx
+++ src/components/Shortcut.jsx
@@ -1,10 +1,12 @@
 const Shortcut = ({ prompt, id, response }) => {
   return (
     <div
-      className={`shortcut ${id} text-left w-11/12 md:w-[640px] lg:w-[800px] p-4 md:p-6 m-1.5 md:m-3 bg-slate-700 rounded-lg flex flex-col text-center`}
+      className={`shortcut ${id} text-left w-11/12 md:w-[640px] lg:w-[800px] p-4 md:p-6 m-1.5 md:m-3 bg-slate-100 dark:bg-slate-700 rounded-lg flex flex-col text-center`}
     >
-      <p className="text-slate-300 text-xs md:text-sm">{prompt}</p>
-      <p className="text-slate-100 font-bold text-sm md:text-base mt-2">
+      <p className="text-slate-700 dark:text-slate-300 text-xs md:text-sm">
+        {prompt}
+      </p>
+      <p className="text-slate-900 dark:text-slate-100 font-bold text-sm md:text-base mt-2">
         {response}
       </p>
     </div>
